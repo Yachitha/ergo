e13f09f1eb2b8ce73b3cf2d80ba015ac02b095f0
Yachitha <yachithasandaruwan@gmail.com>
2018-06-09 21:10:19 +0530

    added delete project view

--- resources/views/Projects/deleteProjects.blade.php
+++ resources/views/Projects/deleteProjects.blade.php
@@ -15,7 +15,7 @@
                     @endif
                 </li>
                 <li class="breadcrumb-item active">
-                    Edit Projects
+                    Delete Projects
                 </li>
             </ol>
             <div class="container">
@@ -27,87 +27,114 @@
                         </button>
                     </div>
                 @endif
-                <div class="row" id="project_edit" style="display: none;">
-                    <div class="col-lg-6">
-                        <div class="card bg-light">
-                            <div class="card-header">
-                                <p style="font-size: 20px;">Edit</p>
-                            </div>
-                            <div class="card-body">
-                                <form class="form-horizontal" action="/saveEditProject" method="POST">
-                                    <input type="hidden" id="project_id" name="project_id" value="">
-                                    <div class="form-group">
-                                        <label for="project_name">Project Name:</label>
-                                        <input type="text" class="form-control" name="name" id="project_name" placeholder="">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="project_description">Project Description:</label>
-                                        <textarea class="form-control" name="description" id="project_description" cols="30" rows="6" placeholder=""></textarea>
-                                    </div>
-                                    <button class="btn btn-success" type="submit">Update</button>
-                                </form>
-                            </div>
+                @if (session('error'))
+                        <div class="alert alert-danger alert-dismissible fade show" id="session_id">
+                            {{ session('error') }}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
-                    </div>
-                    <div class="col-lg-5 offset-1">
-                        <div class="card bg-light">
-                            <div class="card-header">
-                                <p style="font-size: 20px;">Extend Deadline</p>
-                            </div>
-                            <div class="card-body">
-                                <p style="font-style: italic; font-size: 15px; color: rgba(24,29,24,0.89); font-weight: bold;">Extending of deadline will affect scheduled time line of the company!</p>
-                                <form class="form-horizontal" action="/extendDeadline" method="POST">
-                                    <input type="hidden" id="extend_id" name="project_id" value="">
-                                    <div class="form-group">
-                                        <label for="end_date">End Date</label>
-                                        <input type="text" class="form-control" readonly placeholder="" id="endDate">
-                                    </div>
-                                    <div class="form-group">
-                                        <button id="extend_request" class="btn btn-danger" type="button">Extend deadline</button>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="date" class="form-control" id="extended_date" name="extended_date" placeholder="yyyy/mm/dd" style="display: none;">
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-outline-success" id="continue_btn" style="display: none;">continue</button>
-                                    </div>
-                                </form>
-                            </div>
+                @endif
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card bg-light" style="border-top: 4px solid #ff6761;">
+                        <div class="card-body">
+                            <table class="table table-responsive table-hover">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Project Name</th>
+                                    <th scope="col">Category</th>
+                                    <th scope="col">Start Date</th>
+                                    <th scope="col">End Date</th>
+                                    <th scope="col"><i class="fa fa-trash-o"></i></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @php
+                                    $j=0
+                                @endphp
+                                @foreach($Cdata as $data)
+                                    <tr>
+                                        <th scope="row">{{ ++$j }}</th>
+                                        <td>{{ $data->name }}</td>
+                                        <td>{{ $data->category }}</td>
+                                        <td>{{ $data->start_date }}</td>
+                                        <td>{{ $data->end_date }}</td>
+                                        <td>
+                                        @if($data->status)
+                                            @if($data->status== "finish")
+                                                <button class="btn btn-sm btn-success" data-toggle="modal" data-target="#deleteProject"><i class="fa fa-trash-o"></i></button>
+                                            @else
+                                                <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteProject_noStatus"><i class="fa fa-trash-o"></i></button>
+                                            @endif
+                                        @else
+                                            <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteProject_noStatus"><i class="fa fa-trash-o"></i></button>
+                                        @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
-                <div class="row" style="margin-top: 10px;">
-                    <div class="col-lg-12">
-                        <div class="card bg-light" style="border-top: 4px solid #ff6761;">
-                            <div class="card-body">
-                                <table class="table table-responsive table-hover">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Project Name</th>
-                                        <th scope="col">Start Date</th>
-                                        <th scope="col">End Date</th>
-                                        <th scope="col"><i class="fa fa-pencil-square-o"></i></th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @php
-                                        $j=0
-                                    @endphp
-                                    @foreach($Cdata as $data)
-                                        <tr id="row_id">
-                                            <th scope="row">{{ ++$j }}</th>
-                                            <td>{{ $data->name }}</td>
-                                            <td>{{ $data->start_date }}</td>
-                                            <td>{{ $data->end_date }}</td>
-                                            <td><button class="btn btn-sm" onclick="projectUpdate( '{{ $data->id }}' , '{{ $data->name }}', '{{ $data->description }}', '{{ $data->start_date }}', '{{ $data->end_date }}' )"><i class="fa fa-pencil-square-o"></i></button></td>
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="deleteProject" tabindex="-1" role="dialog" aria-labelledby="deleteProject" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-center" style="color: rgba(24,29,24,0.71);">Sure want to delete!</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p style="color: #ff6761; font-size: 15px;">This project updated as finished.</p>
+                    <form action="/deleteProject" method="post" id="projectDeletion">
+                        <div class="form-group">
+                            <label for="reason">Reason:</label>
+                            <input type="text" class="form-control" placeholder="provide a reason in short" name="reason" id="reason" value="">
+                            <input type="hidden" name="project_id" id="hiddenValue" value="" />
+                            <small style="display: none; color: #ff6953;" id="validateInput">Please provide a short reason like: finished, decline or other</small>
                         </div>
-                    </div>
+                    </form>
+                    <p style="color: #454d45; font-size: 13px;">*The project deletion will notify to the CEO</p>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <button class="btn btn-danger" type="submit" onclick="validateForm()">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="deleteProject_noStatus" tabindex="-1" role="dialog" aria-labelledby="deleteProject_noStatus" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-center" style="color: rgba(24,29,24,0.71);">Sure want to delete!</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p style="color: #ff6761; font-size: 15px;">This project not updated as finished. Deleting this project will loose all the details of this project!</p>
+                    <form action="/deleteProject" method="post" id="projectDeletion">
+                        <div class="form-group">
+                            <label for="reason">Reason:</label>
+                            <input type="text" class="form-control" placeholder="provide a reason in short" name="reason" id="reason" value="">
+                            <input type="hidden" name="project_id" id="hiddenValue" value="" />
+                            <small style="display: none; color: #ff6953;" id="validateInput">Please provide a short reason like: finished, decline or other</small>
+                        </div>
+                    </form>
+                    <p style="color: #454d45; font-size: 13px;">*The project deletion will notify to the CEO</p>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <button class="btn btn-danger" type="submit" onclick="validateForm()">Delete</button>
                 </div>
             </div>
         </div>
