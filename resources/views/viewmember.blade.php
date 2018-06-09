2a40a740e12581afafd59c802532aaaf1c180c0e
Yachitha <yachithasandaruwan@gmail.com>
2018-06-09 21:13:54 +0530

    change the view

--- resources/views/viewmember.blade.php
+++ resources/views/viewmember.blade.php
@@ -1,82 +1,59 @@
 @extends('layout')
   @section('content')
-<!DOCTYPE html>
-<html lang="en">
-  <div class="content-wrapper" style="background-image: url(background1.jpg);">
-    <div class="container-fluid">
-      <!-- Breadcrumbs-->
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-          <a href="/ssindex">Dashboard</a>
-        </li>
-        <li class="breadcrumb-item active">Tasks</li>
-      </ol>
-      <div class="row">
-        <div class="col-12"> 
-          <h1>Tasks</h1>
-          <p>This is an example of a blank page that you can use as a starting point for creating new ones.</p>
-          <hr>
-          <div class="container">
-
-            <div class="row">
-            <?php
-            foreach ($Cdata as $key){
-            	# code...
-            	echo "<div class=\"card \" style=\"width: 18rem; margin-right: 10px; margin-top: 10px\">
-			  <img class=\"card-img-top\" src=\"";
-			  echo $key ->profile_pic ;
-			  echo "\" alt=\"Card image cap\">
-			  <div class=\"card-body\">
-			    <h5 class=\"card-title\">";
-			    echo $key->name;
-			    echo "</h5>
-			    <p class=\"card-text\">E-mail-";
-			    echo $key->email;
-			    echo "</br>";
-			    echo $key->role;
-			    echo "</br>";
-			    if($key->status) 
-			    	echo "currently assigned";
-			    else
-			    	 echo "currenlty available" ;
-			    echo "</p>
-			    <a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>
-			  </div>
-			</div>";
-
-            }
-            ?>
-              <div class="card" style="width: 18rem; margin-right: 5px; margin-right: 5px">
-			  <img class="card-img-top" src="..." alt="Card image cap">
-			  <div class="card-body">
-			    <h5 class="card-title">Card title</h5>
-			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-			    <a href="#" class="btn btn-primary">Go somewhere</a>
-			  </div>
+	  <div class="content-wrapper" style="background-image: url(background1.jpg);">
+		  <div class="container-fluid">
+			  <!-- Breadcrumbs-->
+				  <ol class="breadcrumb">
+				  <li class="breadcrumb-item">
+					  <a href="/ssindex">Dashboard</a>
+				  </li>
+				  <li class="breadcrumb-item active">Tasks</li>
+				  </ol>
+			  <div class="row">
+				  	<div class="col-12">
+					  <h1>View all Employees</h1>
+          				<hr>
+				  	<div class="container">
+					  	<div class="row">
+							@foreach ($Cdata as $key)
+							<div class="col-md-3" style="margin-left: 10px; margin-top: 10px;">
+								<div class="card bg-light" style="border-top: 4px solid #c0503e;">
+									<div class="card-header text-center">
+									  <img src={{ url('/') }}/uploads/profiles/{{ $key->profile_pic }} alt="profile_picture" style="width: 100px; height:100px; position: center; border-radius: 50%; margin-top: 10px;">
+									</div>
+									<div class="card-body text-center">
+										<h6 class="card-text">
+											{{ $key->name }}
+										</h6>
+										<p class="card-text">
+											{{ $key->email }}
+										</p>
+										<p class="card-text">
+											{{ $key->role }}
+										</p>
+										@if ($key->status)
+											<p class="card-text">
+												Currently Assigned
+											</p>
+										@else
+											<p class="card-text">
+												Currently Available
+											</p>
+										@endif
+									</div>
+									<div class="card-footer">
+										<a href="#" class="btn btn-primary">more</a>
+									</div>
+							  </div>
+							</div>
+							@endforeach
+						</div>
+					</div>
+		  		</div>
 			</div>
-
-
-           <div class="row">
-              <div class="card" style="width: 18rem;">
-			  <img class="card-img-top" src="..." alt="Card image cap">
-			  <div class="card-body">
-			    <h5 class="card-title">Card title</h5>
-			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-			    <a href="#" class="btn btn-primary">Go somewhere</a>
-			  </div>
-			</div>
-		</div>
-	</div>
-	</div>
-	</div>
-	</div>
-	</div>
+		  </div>
+	  </div>
 </div>
-
     <!-- /.container-fluid-->
     <!-- /.content-wrapper-->
-   
-
-</html>
-  
 @endsection
