fb4bd220117476a499b4dd98989c530b127a7a39
Yachitha <yachithasandaruwan@gmail.com>
2018-06-10 13:48:01 +0530

    change the view apperance

--- resources/views/viewProjectOne.blade.php
+++ resources/views/viewProjectOne.blade.php
@@ -29,8 +29,11 @@
                     <div class="card-body">
                       <h5 class="card-title">{{ $Cdata->project->name }}</h5>
                       <p class="card-text">{{ $Cdata->project->description }}</p>
-                      <a href="#" class="btn btn-outline-primary">more</a>
-                      <a href="/viewProjects" class="btn btn-outline-primary">back</a>
+                      <form action="/moreOnProject" method="post">
+                        <input type="hidden" name="id" id="moreOnProject_id" value={{ $Cdata->project->id }}>
+                        <button type="submit" class="btn btn-outline-primary">more</button>
+                        <a href="/viewProjects" class="btn btn-outline-primary">back</a>
+                      </form>
                     </div>
                     <div class="card-footer text-muted">
                       <?php
@@ -48,7 +51,31 @@
               <div class="col-md-8">
                   <div class="card bg-light" style="border-top: 4px solid #b28bff; margin-top: 10px; margin-left:auto">
                       <div class="card-body">
-                          skdfsdjgp
+                        <div class="card-text">
+                          <h6>Tasks</h6>    
+                        </div>
+                          <ul class="list-group list-group-flush">
+                            @if($Cdata->tasks)
+                              @php
+                                  $i=1;
+                              @endphp
+                              @foreach($Cdata->tasks as $task)
+                                <li class="list-group-item" onclick="showDetails('{{ $task->id }}')" style="background-color: #b28bff;"><i class="fa fa-plus" id=icon_{{ $task->id }} style="margin-right: 4px;"></i>{{ $task->name }}</li>
+                                <div class="card-text" style="display: none;" id={{ $task->id }}>
+                                  <ul class="list-group list-group-flush">
+                                    <li class="list-group-item"><h6>Description: </h6>{{ $task->description }}</li>
+                                    <li class="list-group-item"><h6>Start Date: </h6>{{ $task->start_date }}</li>
+                                    <li class="list-group-item"><h6>End Date: </h6>{{ $task->end_date }}</li>
+                                  </ul>
+                                </div>
+                                @php
+                                  ++$i;
+                                @endphp
+                              @endforeach
+                            @else
+                              <li class="list-group-item bg-warning">No tasks were created yet!</li>
+                            @endif        
+                          </ul>
                       </div>
                   </div>
               </div>
